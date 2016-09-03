0c48c47e7802cea7819c9da19e57ed7326b80f9e
Alexander Kravets <a@kra.vc>
2016-09-03 09:29:54 +0300

    Add show page content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,21 @@ module ApplicationHelper
       #'cancelled'    => 'error',
     }[status] || ''
   end
+
+  def amount(value, currency)
+    currency = currency_format(currency)
+    number_to_currency(value, unit: currency)
+  end
+
+  def tax_label(value)
+    value ? 'з ПДВ' : 'без ПДВ'
+  end
+
+  def tender_json_path(id)
+    "http://api.openprocurement.org/api/2.3/tenders/#{id}"
+  end
+
+  def tender_promua_path(id)
+    "http://zakupki.prom.ua/dz/list?search=#{id}"
+  end
 end
